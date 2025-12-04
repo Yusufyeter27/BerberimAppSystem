6c2759e593ad09a06bbcbddccb1f920250fec681
Ysfytr27 <yusufyeter836@gmail.com>
2025-12-04 19:29:46 +0300

    anasayfa ksımı düzenleme yapıldı

--- src/application/AnaSayfaController.java
+++ src/application/AnaSayfaController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
 public class AnaSayfaController {
 
@@ -15,7 +17,32 @@ public class AnaSayfaController {
     private URL location;
 
     @FXML
+    private MenuItem cikisyap;
+
+    @FXML
+    private MenuItem profilim;
+
+    @FXML
+    private Label profiltext;
+
+    @FXML
     private Button randevuAlTiklandi;
+
+    @FXML
+    private MenuItem randevularim;
+
+    @FXML
+    private Label sac;
+
+    @FXML
+    void cikisyapclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void profilimclick(ActionEvent event) {
+
+    }
 
     @FXML
     void randevuAlTiklandi(ActionEvent event) {
@@ -23,8 +50,18 @@ public class AnaSayfaController {
     }
 
     @FXML
+    void randevularimclick(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert cikisyap != null : "fx:id=\"cikisyap\" was not injected: check your FXML file 'AnaSayfa.fxml'.";
+        assert profilim != null : "fx:id=\"profilim\" was not injected: check your FXML file 'AnaSayfa.fxml'.";
+        assert profiltext != null : "fx:id=\"profiltext\" was not injected: check your FXML file 'AnaSayfa.fxml'.";
         assert randevuAlTiklandi != null : "fx:id=\"randevuAlTiklandi\" was not injected: check your FXML file 'AnaSayfa.fxml'.";
+        assert randevularim != null : "fx:id=\"randevularim\" was not injected: check your FXML file 'AnaSayfa.fxml'.";
+        assert sac != null : "fx:id=\"sac\" was not injected: check your FXML file 'AnaSayfa.fxml'.";
 
     }
 
