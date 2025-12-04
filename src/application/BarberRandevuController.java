d04d72b10912dabc368334e5e0d469d7161ccac2
Ysfytr27 <yusufyeter836@gmail.com>
2025-12-04 18:54:46 +0300

    Berber randevu arayüzü tamamlandı:)

---
+++ src/application/BarberRandevuController.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class BarberRandevuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void emrebutonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kadirbutonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void yusufbutonclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
