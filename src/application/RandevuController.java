eafaa237b53f3544e91fc3192ac866376a97aacb
Ysfytr27 <yusufyeter836@gmail.com>
2025-12-05 18:06:30 +0300

    berber1 kısmını düzenliyorum

--- src/application/RandevuController.java
+++ src/application/RandevuController.java
@@ -3,9 +3,12 @@ package application;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
 
 public class RandevuController {
 
@@ -16,18 +19,40 @@ public class RandevuController {
     private URL location;
 
     @FXML
+    private Text RandevuIsmi;
+
+    @FXML
+    private MenuItem cikisyap;
+
+    @FXML
+    private ToggleGroup personelgrup;
+
+    @FXML
+    private MenuItem profilim;
+
+    @FXML
     private DatePicker randevuTarihi;
 
-    public void initialize(URL location, ResourceBundle resources) {
-        // Bugünden önceki tarihleri kapatma ayarı
-        randevuTarihi.setDayCellFactory(picker -> new DateCell() {
-            @Override
-            public void updateItem(LocalDate date, boolean empty) {
-                super.updateItem(date, empty);
-                // Eğer tarih bugünden önceyse pasif yap
-                setDisable(empty || date.compareTo(LocalDate.now()) < 0);
-            }
-        });
+    @FXML
+    private MenuItem randevularim;
+
+    @FXML
+    void cikisyapclick(ActionEvent event) {
+
     }
 
+    @FXML
+    void profilimclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void randevularimclick(ActionEvent event) {
+
+    }
+    public void initialize(URL location, ResourceBundle resources) {
+         //UNUTMA EMOŞŞ TARİH SEÇİM KISMINI
+    }
 }
+
+
